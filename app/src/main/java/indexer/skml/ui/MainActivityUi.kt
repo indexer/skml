0d94ffdebf6696f035c193dc07d84814a4c62747
indexer <yemonkyaw007@gmail.com>
2016-09-12 15:07:32 +0630

    added on click

--- app/src/main/java/indexer/skml/ui/MainActivityUi.kt
+++ app/src/main/java/indexer/skml/ui/MainActivityUi.kt
@@ -2,6 +2,7 @@ package indexer.skml.ui
 
 import android.text.InputType.TYPE_CLASS_TEXT
 import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
+import android.view.Gravity
 import indexer.skml.MainActivity
 import indexer.skml.R
 import org.jetbrains.anko.*
@@ -10,18 +11,35 @@ import org.jetbrains.anko.*
  * Created by indexer on 9/12/16.
  */
 class MainActivityUi : AnkoComponent<MainActivity> {
-    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-        verticalLayout {
-            padding = dip(resources.getDimension(R.dimen.activity_horizontal_margin))
+  override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+    verticalLayout {
+      gravity = Gravity.CENTER_VERTICAL
+      backgroundColor = R.color.colorAccent
+      padding = dip(resources.getDimension(R.dimen.activity_horizontal_margin))
 
-            val name = editText {
-                hintResource = R.string.app_name
-            }
+      val userName = editText {
+        hintResource = R.string.app_name
+        textResource = R.string.app_name
+      }
 
-            val password = editText {
-                hintResource = R.string.password
-                inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD
-            }
+
+      val userPassword = editText {
+        hintResource = R.string.password
+        inputType = TYPE_CLASS_TEXT or TYPE_TEXT_VARIATION_PASSWORD
+      }
+
+      val loginButton = button {
+        textResource = R.string.login
+        lparams {
+          width = matchParent
+          height = wrapContent
         }
+        onClick {
+          toast("Hello,${userName.text}")
+        }
+      }
     }
+
+  }
 }
+
